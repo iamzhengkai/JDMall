65517cdd295e66abd72ef48538f9336f16c45397
kevin <iamzhengkai@163.com>
2017-06-05 19:43:31 +0800

    测试 添加空行

--- app/src/main/java/com/kevin/jdmall/utils/ToastUtil.java
+++ app/src/main/java/com/kevin/jdmall/utils/ToastUtil.java
@@ -11,11 +11,13 @@ public class ToastUtil {
 	 * @param text
 	 */
 	public static void showToast(String text){
+		
 		if(toast==null){
 			toast = Toast.makeText(MyApplication.getContext(), text,Toast.LENGTH_SHORT);
 		}else {
 			toast.setText(text);//如果不为空，则直接改变当前toast的文本
 		}
 		toast.show();
+	
 	}
 }
