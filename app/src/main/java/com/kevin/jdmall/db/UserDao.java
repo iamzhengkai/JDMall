58cfe47e936bf2f0b17427d93be5a877d7a73c19
kevin <iamzhengkai@163.com>
2017-06-09 19:03:35 +0800

    修改数据库异步为RxJava

--- app/src/main/java/com/kevin/jdmall/db/UserDao.java
+++ app/src/main/java/com/kevin/jdmall/db/UserDao.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.kevin.jdmall.MyApplication;
 import com.kevin.jdmall.MyConstants;
 import com.kevin.jdmall.bean.User;
 
@@ -15,48 +14,55 @@ import com.kevin.jdmall.bean.User;
 
 public class UserDao {
     private SQLiteOpenHelper mHelper;
-    public UserDao(){
+   /* private SqlBrite mSqlBrite;
+    private BriteDatabase db;
+    private Observable<SqlBrite.Query> mUserObservable;*/
+
+    public UserDao() {
         mHelper = new DbOpenHelper();
+      /*  mSqlBrite = new SqlBrite.Builder().build();
+        db = mSqlBrite.wrapDatabaseHelper(mHelper, Schedulers.io());
+        mUserObservable = db.createQuery(MyConstants.DbConstants.TBL_NAME, "SELECT * FROM " + MyConstants.DbConstants.TBL_NAME);*/
     }
-    public  boolean addUser(String username,String pwd){
-       /* String sql = "insert into " + MyConstants.DbConstants.TBL_NAME +"(" +
-                MyConstants.DbConstants.CLM_USERNAME + "," + MyConstants.DbConstants.CLM_PWD +")" +
-                "values(" + username + "," + pwd + ");";
-        mHelper.getWritableDatabase().execSQL(sql);*/
+
+    public boolean addUser(String username, String pwd) {
 
         SQLiteDatabase database = mHelper.getWritableDatabase();
+
         ContentValues values = new ContentValues();
         values.put(MyConstants.DbConstants.CLM_USERNAME,username);
         values.put(MyConstants.DbConstants.CLM_PWD,pwd);
         long resultCount = database.insert(MyConstants.DbConstants.TBL_NAME, null, values);
         database.close();
+
         return resultCount != -1;
+
+       /* ContentValues values = new ContentValues();
+        values.put(MyConstants.DbConstants.CLM_USERNAME, username);
+        values.put(MyConstants.DbConstants.CLM_PWD, pwd);
+        db.insert(MyConstants.DbConstants.TBL_NAME, values);*/
     }
 
-    public void clearUser(){
+    public void clearUser() {
         SQLiteDatabase database = mHelper.getWritableDatabase();
         database.delete(MyConstants.DbConstants.TBL_NAME, null, null);
         database.close();
     }
 
-    public User getUser(){
+    public User getUser() {
         SQLiteDatabase database = mHelper.getWritableDatabase();
         Cursor cursor = database.query(MyConstants.DbConstants.TBL_NAME, null, null, null, null,
                 null, null);
-
-        if (cursor.getCount() == 0){
+        User user = null;
+        if(cursor.moveToFirst()){
+            user = new User();
+            user._id = cursor.getInt(cursor.getColumnIndex(MyConstants.DbConstants.CLM_ID));
+            user.username = cursor.getString(cursor.getColumnIndex(MyConstants.DbConstants.CLM_USERNAME));
+            user.pwd = cursor.getString(cursor.getColumnIndex(MyConstants.DbConstants.CLM_PWD));
             cursor.close();
             database.close();
-            return null;
         }
 
-        cursor.moveToFirst();
-        User user = new User();
-        user._id = cursor.getInt(cursor.getColumnIndex(MyConstants.DbConstants.CLM_ID));
-        user.username = cursor.getString(cursor.getColumnIndex(MyConstants.DbConstants.CLM_USERNAME));
-        user .pwd = cursor.getString(cursor.getColumnIndex(MyConstants.DbConstants.CLM_PWD));
-        cursor.close();
-        database.close();
         return user;
     }
 }
--- app/src/main/java/com/kevin/jdmall/presenter/impl/LoginPresenterImpl.java
+++ app/src/main/java/com/kevin/jdmall/presenter/impl/LoginPresenterImpl.java
@@ -2,7 +2,6 @@ package com.kevin.jdmall.presenter.impl;
 
 import android.text.TextUtils;
 
-import com.kevin.jdmall.BuildConfig;
 import com.kevin.jdmall.MyApplication;
 import com.kevin.jdmall.api.LoginApi;
 import com.kevin.jdmall.bean.LoginResult;
@@ -15,8 +14,6 @@ import com.orhanobut.logger.Logger;
 import java.lang.ref.WeakReference;
 
 import rx.Observable;
-import rx.Observer;
-import rx.Scheduler;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -34,106 +31,136 @@ public class LoginPresenterImpl extends BasePresenterImpl implements ILoginPrese
     public LoginPresenterImpl(ILoginView loginView) {
         if (loginView == null)
             throw new IllegalArgumentException("ILoginView must not be null");
-
-
-        this.mLoginViewRef = new WeakReference<ILoginView>(loginView);
-//        this.mLoginView = loginView;
+        this.mLoginViewRef = new WeakReference<>(loginView);
     }
 
     @Override
     public void login(final String username, final String password) {
-        if ((mLoginViewRef.get()) != null) {
-            mLoginViewRef.get().showProgressDialog();
-        } else {
-            Logger.e("LoginActivity已被回收");
-            return;
-        }
-        Subscription s = MyApplication.mRetrofit.create(LoginApi.class).login(username, password)
+        mLoginViewRef.get().showProgressDialog();
+        Subscription sLogin = MyApplication.mRetrofit.create(LoginApi.class).login(username, password)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<LoginResult>() {
+                .subscribe(new Subscriber<LoginResult>() {
+                    ILoginView loginView = mLoginViewRef.get();
+
+                    @Override
+                    public void onStart() {
+                        if (loginView == null) {
+                            unsubcrible();
+                            Logger.e("视图已经被回收！");
+                        }
+                    }
+
                     @Override
                     public void onCompleted() {
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        if (mLoginViewRef.get() != null) {
-                            mLoginViewRef.get().hideProgressDialog();
-                            mLoginViewRef.get().showError(e.getMessage());
-                        } else {
-                            Logger.e("LoginActivity已被回收");
-                        }
+                        loginView.hideProgressDialog();
+                        loginView.showError(e.getMessage());
                         e.printStackTrace();
                     }
 
                     @Override
                     public void onNext(LoginResult loginResult) {
-                        if (mLoginViewRef.get() != null) {
-                            mLoginViewRef.get().hideProgressDialog();
-                            if (loginResult.isSuccess()) {
-//                                mLoginViewRef.get().showError("登录成功!");
-                                if (BuildConfig.DEBUG) {
-                                    Logger.d(TAG, loginResult.toString());
-                                }
+                        loginView.hideProgressDialog();
+                        if (loginResult.isSuccess()) {
+//                          mLoginViewRef.get().showError("登录成功!");
+                            Logger.d(TAG, loginResult.toString());
+                            //保存用户信息
+                            saveUserInfo(username, password);
 
-                                //保存用户信息
-                                Subscription sSaveUser = Observable.create(new Observable
-                                        .OnSubscribe<Boolean>() {
-                                    @Override
-                                    public void call(Subscriber<? super Boolean> subscriber) {
-                                        UserDao userDao = new UserDao();
-                                        userDao.clearUser();
-                                        boolean result = userDao.addUser(username, password);
-                                        subscriber.onNext(result);
-                                    }
-                                }).subscribeOn(Schedulers.io())
-                                        .observeOn(AndroidSchedulers.mainThread())
-                                        .subscribe(new Subscriber<Boolean>() {
-                                            @Override
-                                            public void onCompleted() {
-
-                                            }
-
-                                            @Override
-                                            public void onError(Throwable e) {
-                                                if (mLoginViewRef.get() != null) {
-                                                    mLoginViewRef.get().showError(e.getMessage());
-                                                }
-                                            }
-
-                                            @Override
-                                            public void onNext(Boolean isSuccess) {
-                                                if (mLoginViewRef.get() != null) {
-                                                    if (isSuccess) {
-                                                        //跳转到主界面
-                                                        mLoginViewRef.get().showError("登录成功!");
-                                                        mLoginViewRef.get().jumpToMainActivity();
-                                                    } else {
-                                                        mLoginViewRef.get().showError
-                                                                ("保存用户信息失败:数据库错误!");
-                                                    }
-                                                }
-
-                                            }
-                                        });
-
-                                addSubscription(sSaveUser);
-
-                            } else {
-                                mLoginViewRef.get().showError(loginResult.getErrorMsg());
-                            }
                         } else {
-                            Logger.e("LoginActivity已被回收");
+                            loginView.showError(loginResult.getErrorMsg());
                         }
+
                     }
                 });
-        addSubscription(s);
+        addSubscription(sLogin);
+    }
+
+    private void saveUserInfo(final String username, final String password) {
+        /*DbOpenHelper mHelper = new DbOpenHelper();
+        SqlBrite mSqlBrite = new SqlBrite.Builder().build();
+        BriteDatabase db = mSqlBrite.wrapDatabaseHelper(mHelper, Schedulers.io());
+        Observable<SqlBrite.Query> userObservable =
+                db.createQuery(MyConstants.DbConstants.TBL_NAME,
+                        "SELECT * FROM " + MyConstants.DbConstants.TBL_NAME);
+
+        Subscription s = userObservable.subscribe(new Action1<SqlBrite.Query>() {
+            @Override
+            public void call(SqlBrite.Query query) {
+                Cursor cursor = query.run();
+
+                if (cursor.getCount() > 0) {
+                    //跳转到主界面
+                    mLoginViewRef.get().showError("登录成功!");
+                    mLoginViewRef.get().jumpToMainActivity();
+                } else {
+                    mLoginViewRef.get().showError
+                            ("保存用户信息失败:数据库错误!");
+                }
+
+
+            }
+        });
+
+        addSubscription(s);*/
+
+
+        //保存用户信息
+        Subscription sSaveUser = Observable.create(new Observable
+                .OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+                UserDao userDao = new UserDao();
+                userDao.clearUser();
+                boolean result = userDao.addUser(username, password);
+                subscriber.onNext(result);
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Boolean>() {
+                    ILoginView loginView = mLoginViewRef.get();
+
+                    @Override
+                    public void onStart() {
+                        if (loginView == null) {
+                            unsubcrible();
+                            Logger.e("视图已经被回收！");
+                        }
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        loginView.showError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Boolean isSuccess) {
+                        if (isSuccess) {
+                            //跳转到主界面
+                            loginView.showError("登录成功!");
+                            loginView.jumpToMainActivity();
+                        } else {
+                            loginView.showError("保存用户信息失败:数据库错误!");
+                        }
+                    }
+
+                });
+
+        addSubscription(sSaveUser);
     }
 
     @Override
     public void readUserFromDb() {
-        Subscription s = Observable.create(new Observable.OnSubscribe<User>() {
+        Subscription sReadUser = Observable.create(new Observable.OnSubscribe<User>() {
             @Override
             public void call(Subscriber<? super User> subscriber) {
                 UserDao userDao = new UserDao();
@@ -143,6 +170,16 @@ public class LoginPresenterImpl extends BasePresenterImpl implements ILoginPrese
         }).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<User>() {
+                    ILoginView loginView = mLoginViewRef.get();
+
+                    @Override
+                    public void onStart() {
+                        if (loginView == null) {
+                            unsubcrible();
+                            Logger.e("视图已经被回收！");
+                        }
+                    }
+
                     @Override
                     public void onCompleted() {
 
@@ -150,19 +187,17 @@ public class LoginPresenterImpl extends BasePresenterImpl implements ILoginPrese
 
                     @Override
                     public void onError(Throwable e) {
-
+                        e.printStackTrace();
                     }
 
                     @Override
                     public void onNext(User user) {
                         if (user != null) {
-                            if (mLoginViewRef.get() != null){
-                                mLoginViewRef.get().setUserInfo(user.username,user.pwd);
-                            }
+                            loginView.setUserInfo(user.username, user.pwd);
                         }
                     }
                 });
-        addSubscription(s);
+        addSubscription(sReadUser);
     }
 
     @Override
